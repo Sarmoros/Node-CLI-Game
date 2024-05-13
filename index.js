4427e0d963407439c18590f4ab39a19b6792138c
Sarahy M <sarahym0703@gmail.com>
2024-05-13 10:29:54 -0400

    added winner function and handle answer function

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ let playerName;
 const sleep = (ms = 2000 ) => new Promise((r) => setTimeout(r, ms));
 
 // the welcome function is used to display a welcome message to the user
-async function welcome(){
+async function welcome() {
     const rainbowTitle = chalkAnimation.rainbow('Who wants to be a Millionaire? \n'
     );
 
@@ -31,10 +31,9 @@ async function welcome(){
     `);
 }
 
-await welcome();
 
 // the askName function is used to ask the player for their name and store it in the playerName variable
-async function askName(){
+async function askName() {
     const answers = await inquirer.prompt({
         name: 'player_name',
         type: 'input',
@@ -46,4 +45,46 @@ async function askName(){
     playerName = answers.player_name;
 }
 
+async function question1() {
+    const answers = await inquirer.prompt({
+        name: 'question_1',
+        type: 'list',
+        message: 'Javascript was created in 10 days then released on\n',
+        choices: ['May 23rd, 1995',
+            'Nov 24th, 1995',
+            'Dec 4th, 1995',
+            'Sep 1st, 1996',
+        ],
+    });
+
+    return handleAnswer(answers.question_1 == 'Dec 4th, 1995');
+}
+
+
+async function handleAnswer(correct) {
+    const spinner = createSpinner('Checking answer...').start();
+    await sleep();
+
+    if (correct) {
+        spinner.success({ text: `Nice work ${playerName}!`});
+    } else {
+        spinner.error({ text: `💀💀💀 Game Over, you lose ${playerName}!`});
+        process.exit(1);
+    }
+}
+
+function winner() {
+    console.clear();
+    const msg = `Congrats, ${playerName}!`;
+
+    figlet(msg, (err, data) => {
+        console.log(gradient.pastel.multiline(data));
+    })
+}
+
+
+
+await welcome();
 await askName();
+await question1();
+await winner();
